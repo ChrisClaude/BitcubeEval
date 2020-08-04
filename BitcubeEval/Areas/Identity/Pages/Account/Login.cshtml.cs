96652aa0acc7b396b3314e226c49459077be23db
ChClaude <christ.tchambila@gmail.com>
2020-08-04 20:54:09 +0200

    Section 2

--- BitcubeEval/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BitcubeEval/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -79,8 +79,7 @@ namespace BitcubeEval.Areas.Identity.Pages.Account
 
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+
                 var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
